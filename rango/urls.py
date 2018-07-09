712a1a615a104f4755858723ffdde8c7d6596547
jakubwi <jakub_wiatrowski@o2.pl>
2018-07-09 16:43:18 +0200

    after chapter 6 before exercises

--- rango/urls.py
+++ rango/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from rango import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    re_path(r'^category/(?P<category_name_slug>[\w\-]+)/$',
+        views.show_category, name='show_category')
 ]
--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from rango.models import Page, Category
 
 def index(request):
-    context_dict ={'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.order_by('-likes') [:5]
+    context_dict ={'categories': category_list}
     return render(request, 'rango/index.html', context=context_dict)
 def about(request):
     context_dict={'boldmessagetwo': "Na dole zdjecie Diabla!"}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+    try: 
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        context_dict['pages'] = None
+        context_dict['category'] = None
+    return render(request, 'rango/category.html', context_dict)
