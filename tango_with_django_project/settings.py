4792b9932c19764d591d70874580122b3cecb047
jakubwi <jakub_wiatrowski@o2.pl>
2018-07-13 16:21:52 +0200

    after chapter 11

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 ]
 
 MIDDLEWARE = [
@@ -129,4 +130,12 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
-LOGIN_URL = '/rango/login'
+LOGIN_URL = '/accounts/login/'
+
+# Django registration redux
+
+REGISTRATION_OPEN = True
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/rango/'
+LOGIN_URL = '/accounts/login/'
